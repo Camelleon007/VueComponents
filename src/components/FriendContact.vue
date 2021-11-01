58638e8324ed278c4c4b1d9584ea0bd47b0ebf50
Kamil Kowalski <kamil@olicom.pl>
2021-11-01 10:21:32 +0100

    initial commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} details</button>
+      <ul v-if="detailsAreVisible">
+          <li><strong>Phone:</strong>{{friend.phone}}</li>
+          <li><strong>Email:</strong>{{friend.email}}</li>
+      </ul>
+  </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: 
+                {
+                    id: 'beniu',
+                    name: 'Beniamin Wiśniewski',
+                    phone: '1234',
+                    email: 'beniu@gmail.pl',
+                }
+            
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
